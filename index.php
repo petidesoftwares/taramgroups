cf073da4e3b7c1ddc744d9631dee1bc6ce42d22e
petidesoftwares <petideenterprise@gmail.com>
2021-05-15 23:37:06 +0100

    progress update

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="taramgroup-css/taramgroup-base-style.css">
+    <link rel="stylesheet" href="taramgroup-css/taramgroups.css">
+    <title>Home</title>
+</head>
+<body>
+    <div class ="row" id ="header">
+        <div class="col-1">
+            <div class ="col-12" id ="logo-pane"></div>
+        </div>
+        <div class = "col-12" id = "header-name">TARAM GROUP OF COMPANIES</div>
+    </div>
+</body>
+</html>
